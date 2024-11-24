0f4db197be027a2b1fc7c3fe2e289252b2314999
madurangaPrabhath <139701327+madurangaPrabhath@users.noreply.github.com>
2024-11-24 14:53:52 +0000

    Add state management

--- practical/src/App.js
+++ practical/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Child from './component/Child';
+import { useState } from 'react';
 
 function App() {
+  const [childname, setChildname] = useState("Default");
+  const receiveName = (name) => {
+    setChildname(name);
+  }
+
   return (
     <div className="App">
-      <Child />
+      <h2>Hello React I'm Parent</h2>
+      <Child setfun={receiveName} />
+      <p>Parent:My child said:{childname}</p>
     </div>
   );
 }
